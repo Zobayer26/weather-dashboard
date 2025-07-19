a99443ad070754fa04bb4a6e92c2ad8a2c7262fa
Zobayer26 <zobayerarif126@gmail.com>
2025-07-19 06:56:52 +0600

    Add Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import Header from "./components/Header/Header"
 
 
 function App() {
 
 
   return (
-    <div className="container">
-      welcome
+    <div>
+      <Header />
     </div>
   )
 }
